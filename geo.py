c2b550132b9e37ccb67b85657a896361015732f1
zec0816 <tzecv0422@gmail.com>
2025-08-10 20:22:23 +0800

    Update latest rankings with rank nums

--- geo.py
+++ geo.py
@@ -2,53 +2,37 @@ import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 
+# Read Excel
 data = pd.read_excel('players_20250803.xlsx', sheet_name='players_20250803')
 
-geolocator = Nominatim(user_agent="geoapiExercises")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+# Use a unique user_agent with contact info
+geolocator = Nominatim(user_agent="renju_rating_geocoder (trash5896@gmail.com)")
+
+# Add delay to respect rate limits
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
+
+# Cache results to avoid duplicate lookups
+cache = {}
 
 def get_coordinates(city, country):
+    key = (city, country)
+    if key in cache:
+        return cache[key]
     try:
         location = geolocator.geocode(f"{city}, {country}")
         if location:
-            return location.latitude, location.longitude
+            coords = (location.latitude, location.longitude)
         else:
-            return None, None
-    except:
-        return None, None
+            coords = (None, None)
+    except Exception:
+        coords = (None, None)
+    cache[key] = coords
+    return coords
 
-data[['latitude', 'longitude']] = data.apply(lambda row: pd.Series(get_coordinates(row['City'], row['Country'])), axis=1)
-
-data.to_excel('players_with_coordinates_20250803.xlsx', sheet_name='players_20250803.csv', index=False)
-
-"""
-If still not complete due to GeoAPI limitation, run this again:
-
-import pandas as pd
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
-
-data = pd.read_excel('players_with_coordinates.xlsx', sheet_name='players')
-
-geolocator = Nominatim(user_agent="geoapiExercises")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-
-def get_coordinates(city, country):
-    try:
-        location = geolocator.geocode(f"{city}, {country}")
-        if location:
-            return location.latitude, location.longitude
-        else:
-            return None, None
-    except:
-        return None, None
-
+# Apply coordinate fetching
 data[['latitude', 'longitude']] = data.apply(
-    lambda row: pd.Series(get_coordinates(row['City'], row['Country'])) 
-    if pd.isnull(row['latitude']) or pd.isnull(row['longitude']) else pd.Series([row['latitude'], row['longitude']]), 
-    axis=1
+    lambda row: pd.Series(get_coordinates(row['City'], row['Country'])), axis=1
 )
 
-data.to_excel('players_with_coordinates_updated.xlsx', sheet_name='players', index=False)
-
-"""
+# Save result
+data.to_excel('players_with_coordinates_20250803.xlsx', sheet_name='players_20250803', index=False)
